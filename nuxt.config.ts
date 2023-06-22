8da16bc2f23508bc3aae6822db3dc9e0cf217f77
freewaying <freewaying@gmail.com>
2023-06-22 10:02:38 +0800

    chore: add package: tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Demo',
+      titleTemplate: '%s - Nuxt Demo',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
   build: {
     transpile:
@@ -18,6 +40,13 @@ export default defineNuxtConfig({
         process.env.NODE_ENV === 'development'
           ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
           : []
+    }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
     }
   }
 })
